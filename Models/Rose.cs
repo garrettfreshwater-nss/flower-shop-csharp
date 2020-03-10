fa1f5d25d0617e846c2d4c88f3dc3f5d53d0c638
garrettfreshwater-nss <gfresh.nss@gmail.com>
2020-03-10 09:38:17 -0500

    first commit

---
+++ Models/Rose.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+namespace flower_shop
+{
+    public class Rose
+    {
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Cost { get; set; }
+        public string WaterSchedule { get; set; }
+
+        public bool Thorny { get; set; }
+
+        public bool Fragrant { get; set; }
+
+        public override string ToString()
+        {
+            return $"A Fanciful {Color} rose with that costs ${Cost}!";
+        }
+
+        // The collection of exercises that the student is currently working on
+        // public List<Exercise> Exercises = new List<Exercise>();
+
+        // public Rose(string color, string name, string type, int cost, string waterschedule, bool thorny, bool fragrant)
+        // {
+        //     Color = color;
+        //     Name = name;
+        //     Type = type;
+        //     Cost = cost;
+        //     WaterSchedule = waterschedule;
+        //     Thorny = thorny;
+        //     Fragrant = fragrant;
+        // }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace flower_shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rose darkBlueRose = new Rose();
+
+            // Rose myRose = new Rose("Red", "Swisha", "Rose", 35, "Bi-Daily", true, true);
+
+            Console.WriteLine(darkBlueRose);
+        }
+    }
+}
